1c7953661e8f51cc519e64bc7093ba70af75f5e8
PS/2014/041 <rasikawg_ps14041@kln.ac.lk>
2018-10-05 14:53:49 +0530

    q1 q2 ok

---
+++ Practical5/Practical5/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practical5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int num, sum = 0, i = 1;
+
+            num = int.Parse(txt_number.Text);
+            while (i <= num) {
+                sum += i;
+                i++;
+            }
+
+            MessageBox.Show("sum of first "+num+" natural number is "+sum+" sum");
+        }
+    }
+}
